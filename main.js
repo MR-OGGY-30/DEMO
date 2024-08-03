035ecb05506de37cd35001b7c2793dd5634cd40b
MR-OGGY-30 <adhikaryutsa.30@gmail.com>
2024-08-03 17:40:51 +0530

    updating files

---
+++ main.js
@@ -0,0 +1,11 @@
+var input1 = document.getElementById("input1").value;
+var input2 = document.getElementById("input2").value;
+var output = input1 + input2;
+document.getElementById("output").innerHTML = "hello";
+window.alert("hello oggy")
+let a, b, c;
+a = 10;
+b = 20;
+c = 30;
+let x = a + b + c;
+document.getElementById("output2").innerHTML = x;
